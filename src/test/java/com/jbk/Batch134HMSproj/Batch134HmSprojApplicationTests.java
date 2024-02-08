cd34b6e0d81affb0622a053761e822cc0e57ea0b
Nitin <np303304@gmail.com>
2024-02-08 19:15:41 +0530

    first commit

---
+++ src/test/java/com/jbk/Batch134HMSproj/Batch134HmSprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbk.Batch134HMSproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batch134HmSprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
